a02caaadd5b1f9f27ecfe26a62f08f0a406f255b
Benedikt Neuhold <benedikt@webionic.at>
2016-11-23 18:44:31 +0100

    Added a new View for Projection (FrontPage)

--- ResourceManifest.cs
+++ ResourceManifest.cs
@@ -16,12 +16,15 @@ namespace Rimango.Gallery
             manifest.DefineStyle("Rimango.Gallery.Overview").SetUrl("Gallery.Overview.css");
             manifest.DefineStyle("PrettyPhoto").SetUrl("prettyPhoto.css");
             manifest.DefineStyle("Rimango.Gallery.Detail").SetUrl("Gallery.Detail.css");
+            manifest.DefineStyle("Rimango.Gallery.FrontPage").SetUrl("Gallery.FrontPage.css");
 
             manifest.DefineScript("Rimango.Gallery.Detail").SetUrl("Gallery.Detail.js").SetDependencies("jQuery");
             manifest.DefineScript("Rimango.Gallery.Overview").SetUrl("Gallery.Overview.js").SetDependencies("jQuery");
             manifest.DefineScript("PrettyPhoto").SetUrl("PrettyPhoto/jquery.prettyPhoto.js").SetDependencies("jQuery");
             manifest.DefineScript("Masonry").SetUrl("Masonry/masonry.pkgd.min.js").SetDependencies("jQuery").SetDependencies("ImagesLoaded");
             manifest.DefineScript("ImagesLoaded").SetUrl("ImagesLoaded/imagesloaded.pkgd.min.js").SetDependencies("jQuery");
+
+            
         }
     }
 }
